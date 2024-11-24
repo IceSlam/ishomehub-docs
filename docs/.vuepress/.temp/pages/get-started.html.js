ba0144209b4c9d30df9a1c9bd946f97f86d7ff53
IceSlam <pk@noosoft.ru>
2024-11-25 00:23:23 +0300

    fix

--- docs/.vuepress/.temp/pages/get-started.html.js
+++ docs/.vuepress/.temp/pages/get-started.html.js
@@ -1,5 +1,5 @@
 import comp from "/home/iceslam/PhpstormProjects/ishomehub-docs/docs/.vuepress/.temp/pages/get-started.html.vue"
-const data = JSON.parse("{\"path\":\"/get-started.html\",\"title\":\"Быстрый старт\",\"lang\":\"ru-RU\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"Технические характеристики\",\"slug\":\"технические-характеристики\",\"link\":\"#технические-характеристики\",\"children\":[]},{\"level\":2,\"title\":\"Комплектация\",\"slug\":\"комплектация\",\"link\":\"#комплектация\",\"children\":[]}],\"git\":{},\"filePathRelative\":\"get-started.md\"}")
+const data = JSON.parse("{\"path\":\"/get-started.html\",\"title\":\"Быстрый старт\",\"lang\":\"ru-RU\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"Технические характеристики\",\"slug\":\"технические-характеристики\",\"link\":\"#технические-характеристики\",\"children\":[]},{\"level\":2,\"title\":\"Комплектация\",\"slug\":\"комплектация\",\"link\":\"#комплектация\",\"children\":[]}],\"git\":{\"updatedTime\":1732481838000,\"contributors\":[{\"name\":\"IceSlam\",\"email\":\"pk@noosoft.ru\",\"commits\":1,\"url\":\"https://github.com/IceSlam\"}]},\"filePathRelative\":\"get-started.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
